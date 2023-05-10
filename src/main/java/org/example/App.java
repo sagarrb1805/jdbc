1b3f981d55ee0b8f9d61d6bb818707e5b083689d
DonaMaria <donamariajohn123@gmail.com>
2023-05-10 12:01:07 +0530

    first two

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,5 @@
 package org.example;
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 /**
  * Hello world!
@@ -6,12 +7,50 @@ import java.sql.*;
  */
 public class App 
 {
+    public void findRest(String location, Statement statement) throws SQLException {
+        String query = String.format("SELECT res_name FROM restaurant WHERE res_location = '%S'", location);
+
+//        String query = "SELECT res_name FROM restaurant WHERE res_location IN ("+location+ ")";
+        System.out.println(query);
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()){
+            System.out.println(resultSet.getString(1));
+        }
+    }
+    public void loadMenu(String res, Statement statement) throws SQLException {
+
+        String getResidQuery = String.format("SELECT res_id FROM restaurant WHERE res_name = '%s' ", res);
+
+        String Query = String.format("SELECT food_item FROM menu WHERE res_id = (SELECT res_id FROM restaurant WHERE res_name = '%s' )", res);
+
+        ResultSet resultSet = statement.executeQuery(Query);
+        while (resultSet.next()){
+            System.out.println(resultSet.getString(1));
+        }
+    }
     public static void main( String[] args ) throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db", "root", "pass@word1");
+        App app = new App();
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_delivery", "root", "pass@word1");
 
         Statement statement = connection.createStatement();
+
+//        app.findRest("Angamally", statement);
+        app.loadMenu("res 1", statement);
+
+
+
+
+
+
+//        ResultSet resultSet = statement.executeQuery("SELECT * FROM menu");
+////        resultSet.absolute(2);
+//        while (resultSet.next()){
+//            System.out.println(resultSet.getString(2));
+//        }
+
 
     }
 }
